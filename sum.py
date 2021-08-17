8027175d316671935f80cf25c52a1c9294ec9636
Tharun Kumar Srinivasan <tharunkumarsrinivasan@Tharuns-MacBook-Air.local>
2021-08-17 08:54:55 +0530

    Added a new Feature (saying Hello)

--- sum.py
+++ sum.py
@@ -1,7 +1,13 @@
+# Program that returns summation of two numbers
 def add(x, y):
     z = x + y
     return z
 
 result = add(50, 11)
-
 print(result)
+
+# Program that says 'Hello'
+def say_hello(name):
+    print('Hello', name)
+
+say_hello('Tharun Kumar')
